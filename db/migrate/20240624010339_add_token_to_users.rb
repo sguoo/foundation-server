cc9cd95b2091f01bfd106d95ce8abe80bba2c131
sgoo <ppiredragonz@naver.com>
2024-06-24 15:20:10 +0900

    ewqeqw

--- db/migrate/20240624010339_add_token_to_users.rb
+++ db/migrate/20240624010339_add_token_to_users.rb
@@ -1,6 +1,5 @@
-class AddTokenToUsers < ActiveRecord::Migration[6.1]
+class AddTokenToUsers < ActiveRecord::Migration[6.0]
   def change
     add_column :users, :token, :string
-    add_column :users, :verified, :boolean, default: false
   end
 end
